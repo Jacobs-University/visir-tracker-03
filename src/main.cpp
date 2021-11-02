96fa6c22f98b49b5ace95c96a4973e7c3001004f
HannesDiethelm <hannes@diethelm.de>
2021-11-02 16:40:26 +0100

    submission

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,28 @@
 #include "Face.h"
 #include "Marker.h"
 
+using namespace std;
+CascadeClassifier face_cascade("../../data/haarcascade_frontalface_default.xml");
 
+vector<ptr_face_t> detect_faces(const Mat& img) {
+	Mat gray_img;
+	vector<Rect> faces;
+	cvtColor(img, gray_img, COLOR_BGR2GRAY);
+	equalizeHist(gray_img, gray_img);
+	face_cascade.detectMultiScale(gray_img, faces, 1.2, 3);
+	vector<ptr_face_t> res;
+	for (auto face: faces)
+		res.push_back(std::make_shared<CFace>(face));
+	return res;
+}
+
+// Parameters for Shi-Tomasi Corner Detector
+int maxCorners = 128;
+double qualityLevel = 0.01;
+double minDistance = 10;
 
 int main() {
 	
-
 	std::vector<Point2f> vPoints;
 	std::vector<ptr_face_t> vpFaceses;
 	
@@ -25,32 +42,56 @@ int main() {
 
 	controller.init();
 	controller.start();
-	
+
+	// Setup various variables for Lukas-Kanade method
+	Mat old_img, old_grey;
+	vector<Point2f> p0, p1;
+	vector<float> err;
+	vector<uchar> status;
+	TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
+
+	// Getting initial old image and its features
+	old_img = controller.getFrame();
+	goodFeaturesToTrack(old_grey, p0, maxCorners, qualityLevel, minDistance);
+
 	// Main loop
-	Mat img, mask;
+	Mat img, grey, mask;
 	float attenuation = 0.5f;
+	int frame_counter = 10;
 	for(;;) {
 		img = controller.getFrame();
 
 		if (!img.empty()) {
 			if (mask.empty()) mask = Mat(img.size(), img.type());
 			mask.setTo(0);
+			cvtColor(img, grey, COLOR_BGR2GRAY);
 
-			// ------ PUT YOUR CODE HERE -------
-			// vpFaceses = detect faces();
+			// Detecting face and marking it every 10th frame
+			if (frame_counter == 10) {
+				vpFaceses = detect_faces(img);
+				frame_counter = 0;
+
+				// Getting good features to track and visualising
+				goodFeaturesToTrack(grey, p0, maxCorners, qualityLevel, minDistance);
+				CMarker::markPoints(mask, p0, Scalar(255,255,255));
+			}
+			frame_counter++;
 			CMarker::markFaces(mask, vpFaceses);
-			
-			// ------ PUT YOUR CODE HERE -------
-			// vPoints = good features to track ()
-			CMarker::markPoints(mask, vPoints);
-			// { hFlow, vFlow } = calculate optical flow (vPoints)
-			// CMarker::markVecOFF(mask, hFlow, vFlow);
+
+			// Calculating optical flow and visualising
+			vector<Point2f> p2;
+			calcOpticalFlowPyrLK(old_grey, grey, p0, p1, status, err, Size(15,15), 3, criteria);
+			CMarker::markVecOFF(mask, p0, p1, status);
+			for (uint i = 0; i < p0.size(); i++) {
+				if (status[i] == 1) {
+					p2.push_back(p1[i]);
+				}
+			}
+			p0 = p2;
+			old_grey = grey.clone();
 			
 			CMarker::markGUI(mask);
-			
-
 			add(img, attenuation * mask, img);
-
 			imshow("Camera", img);
 		}
 		int key = waitKey(5);
